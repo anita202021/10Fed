df989e37ab7ff207921b9e8c73c9ad3ad44a9a4c
Anita Kushwaha <anita.kushwaha@successive.tech>
2020-07-08 00:24:06 +0530

    runtest

--- src/test/java/RunTests.java
+++ src/test/java/RunTests.java
@@ -3,7 +3,7 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/feature/web/", tags="@Phase1")
+@CucumberOptions(features="src/test/resources/feature/web/")
 //@CucumberOptions(features="src/test/resources/feature/web/facility.feature")
 public class RunTests {
 }
